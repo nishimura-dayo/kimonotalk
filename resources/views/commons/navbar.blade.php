e04305b4652d54e5ae5f3b7ff150ff37b92b0a25
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-11-29 07:51:42 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-light">
+        {{-- トップページへのリンク --}}
+        <a class="site-logo" href="/">KIMONO ＊ TALK</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">会員登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="firstview">
+        <div class="home-icatch">
+            <h1 class="home-headline">着物に関する疑問や悩み、色々あるよね。</h1>
+            <a href="#" class="btn-entry">はじめる</a>
+        </div>
+    </section>
+    <section class="home-menu">
+        <h2 class="home-title">カテゴリから探す</h2>
+        <div class="home-menu-list">
+            <dl class="home-menu-list-item">
+                <dt class="home-category-name"><a href="#" class="home-menu-btn1">着物ショップ</a></dt>
+                <dd class="home-category-text">安心してお買い物しましょ</dd>
+            </dl>
+            <dl class="home-menu-list-item">
+                <dt class="home-category-name"><a href="#" class="home-menu-btn2">着物コーデ</a></dt>
+                <dd class="home-category-text">このコーディネートが好き</dd>
+            </dl>
+            <dl class="home-menu-list-item">
+                <dt class="home-category-name"><a href="#" class="home-menu-btn3">着付けテクニック</a></dt>
+                <dd class="home-category-text">簡単に着る方法ないですか？</dd>
+            </dl>
+            <dl class="home-menu-list-item">
+                <dt class="home-category-name"><a href="#" class="home-menu-btn4">着物姿の芸能人</a></dt>
+                <dd class="home-category-text">おしゃれな芸能人探し</dd>
+            </dl>
+            <dl class="home-menu-list-item">
+                <dt class="home-category-name"><a href="#" class="home-menu-btn5">着物警察</a></dt>
+                <dd class="home-category-text">ある日突然警察に襲われた</dd>
+            </dl>
+            <dl class="home-menu-list-item">
+                <dt class="home-category-name"><a href="#" class="home-menu-btn6">お手入れ・収納</a></dt>
+                <dd class="home-category-text">ドライクリーニングしか駄目？</dd>
+            </dl>
+        </div>
+    </section>
+    <footer>
+        <small>© 2021 webクリエイターの生活</small>
+    </footer>
+@endsection
