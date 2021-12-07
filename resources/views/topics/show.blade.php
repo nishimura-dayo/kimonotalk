d6bd36d609fbe892d112e08a62a45f687fa289b5
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-07 03:11:13 +0000

    topicSinglePage add item

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -8,22 +8,51 @@
         <section class="container">
             <div class="row">
                 <div class="col-sm-8 offset-sm-2">
-                    <div class="topic_content">
-                        <h3>トピック内容↓</h3>
-                        <div class="topic_text">
-                            {{-- トピック詳細--}}
-                            {{ $topic->content }}
+                    <div class="row">
+                        <div class="col-4">
+                            画像表示
+                        </div>
+                        <div class="col-8">
+                            <div class="row">
+                                <div class="col-2">
+                                    {{-- トピック所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                                    <div  class="user-thumbnail"><img src="{{ Gravatar::get($topic->user->email, ['size' => 120]) }}" alt=""></div>
+                                </div>
+                                <div class="col-10">
+                                    <div>
+                                        {{-- トピック所有者のユーザ詳細ページへのリンク --}}
+                                        {!! link_to_route('users.show', $topic->user->name, ['user' => $topic->user->id], ['class' => 'text_link']) !!}
+                                    </div>
+                                    {{-- トピック日時 --}}
+                                    <span class="text-muted">{{ $topic->created_at }}</span>
+                                </div>
+                            </div>
+                            <div class="topic_content">
+                                <div class="topic_text">
+                                    {{-- トピック詳細--}}
+                                    {{ $topic->content }}
+                                </div>
+                                <div class="text-right">
+                                    @if (Auth::id() == $topic->user_id)
+                                        {{-- トピック削除ボタンのフォーム --}}
+                                        {!! Form::open(['route' => ['topics.destroy', $topic->id], 'method' => 'delete']) !!}
+                                            {!! Form::submit('削除する', ['class' => 'btn btn-danger btn-sm']) !!}
+                                        {!! Form::close() !!}
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                     {{-- コメントフォーム --}}
                     @include('topics.form_comment')
                     
                     {{-- コメント一覧 --}}
-                     @include('topics.comments')
+                    @include('topics.comments')
 
-                    {{-- カテゴリ別トピック一覧ページへのリンク --}}
-                    {!! link_to_route('topics.index', 'トピック一覧を見る',[], ['class' => 'text_link']) !!}
+                    <div  class="text-center">
+                        {{-- カテゴリ別トピック一覧ページへのリンク --}}
+                        {!! link_to_route('topics.index', 'トピック一覧を見る',[], ['class' => 'text_link']) !!}
+                    </div>
                 </div>
             </div>
         </section>
