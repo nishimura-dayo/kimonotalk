900ad80e7c5781ff3c7d5cd89b7382c2dfb80dab
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-07 03:09:12 +0000

    topicsList del item

--- resources/views/topics/topics.blade.php
+++ resources/views/topics/topics.blade.php
@@ -1,36 +1,19 @@
 @if (count($topics) > 0)
     <ul class="list-unstyled">
         @foreach ($topics as $topic)
-            <li class="media mb-3">
+            <li class="py-3 border-bottom">
                 {{-- トピック詳細ページへのリンク --}}
-                {{-- {!! link_to_route('topics.show', $topic->id, ['topic' => $topic->id]) !!} --}}
-
-                {{-- トピック所有者のメールアドレスをもとにGravatarを取得して表示 --}}
-                <div  class="user-thumbnail"><img src="{{ Gravatar::get($topic->user->email, ['size' => 120]) }}" alt=""></div>
-
-                <div class="media-body">
-                    <div>
-                        {{-- トピック所有者のユーザ詳細ページへのリンク --}}
-                        {!! link_to_route('users.show', $topic->user->name, ['user' => $topic->user->id], ['class' => 'text_link']) !!}
+                 <a href="{{ route('topics.show', ['topic' => $topic->id]) }}" class="row">
+                    <div class="col-2 topic-thumbnail">
+                        トピック画像
                     </div>
-                    <div>
-                        {{-- トピック詳細ページへのリンク --}}
-                        <a href="{{ route('topics.show', ['topic' => $topic->id]) }}" class="topic_link">
-                            {{-- トピック内容 --}}
-                            <p class="mb-0">{!! nl2br(($topic->content)) !!}</p>
-                            {{-- トピック日時 --}}
-                            <span class="text-muted">{{ $topic->created_at }}</span>
-                        </a>
+                    <div class="col-10">
+                        {{-- トピック内容 --}}
+                        <p class="mb-0">{!! nl2br(($topic->content)) !!}</p>
+                        {{-- トピック日時 --}}
+                        <span class="text-muted">{{ $topic->created_at }}</span>
                     </div>
-                    <div>
-                        @if (Auth::id() == $topic->user_id)
-                            {{-- トピック削除ボタンのフォーム --}}
-                            {!! Form::open(['route' => ['topics.destroy', $topic->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('削除する', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        @endif
-                    </div>
-                </div>
+                </a>
             </li>
         @endforeach
     </ul>
