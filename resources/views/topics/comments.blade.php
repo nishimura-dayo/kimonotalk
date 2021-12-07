3b486d9a3dcf4741972b98df846bd7e0930f1257
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-07 03:07:24 +0000

    comment design change

--- resources/views/topics/comments.blade.php
+++ resources/views/topics/comments.blade.php
@@ -1,7 +1,7 @@
 @if (count($comments) > 0)
-    <ul>
+    <ul class="list-unstyled">
         @foreach ($comments as $comment)
-           <li class="media mb-3">
+           <li class="media py-3 border-bottom">
                 {{-- コメント所有者のメールアドレスをもとにGravatarを取得して表示 --}}
                <div  class="user-thumbnail"><img src="{{ Gravatar::get($comment->user->email, ['size' => 120]) }}" alt=""></div>
                <div class="media-body">
@@ -14,7 +14,7 @@
                         {{-- コメント日時 --}}
                         <span class="text-muted">{{ $comment->created_at }}</span>
                      </div>
-                     <div>
+                     <div class="text-right">
                         @if (Auth::id() == $comment->user_id)
                             {{-- コメント削除ボタンのフォーム --}}
                             {!! Form::open(['route' => ['comments.destroy', $comment->id], 'method' => 'delete']) !!}
--- resources/views/topics/form_comment.blade.php
+++ resources/views/topics/form_comment.blade.php
@@ -1,5 +1,5 @@
 {!! Form::open(['route' => 'comments.store']) !!}
-    <div class="form-graup mb-5">
+    <div class="form-graup mb-3">
         {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '2']) !!}
         {{ Form::hidden('topic_id', $topic->id) }}     
         {!! Form::submit('コメントする', ['class' => 'btn btn-primary btn-block']) !!}
