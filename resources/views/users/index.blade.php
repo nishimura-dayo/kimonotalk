5ca2c84f051c97d1f69b5decc9a69a653e7a6fe3
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-08 07:37:57 +0000

    userPage change Layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,6 +4,7 @@
     <div class="users">
         <h1 class="headline">ユーザ一覧</h1>
 
+
         <section class="container">
             <div class="row">
                 <div class="col-sm-8 offset-sm-2">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,15 +3,21 @@
 @section('content')
     <div class="users container">
         <div class="row">
-            <div class="col-sm-4">
-                {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                <img class="" src="{{ Gravatar::get($user->email, ['size' => 200]) }}">
-                <p>{{ $user->name }}</p>
+            <div class="col-sm-8 offset-sm-2">
+                <div class="row">
+            <div class="col-sm-3">
+                <div class="personal-photo"">
+                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                    <img src="{{ Gravatar::get($user->email, ['size' => 200]) }}">
+                </div>
+                <p class="text-center">{{ $user->name }}</p>
             </div>
-            <div class="col-sm-8">
+            <div class="col-sm-9">
                 {{-- トピック一覧 --}}
                 @include('topics.topics')
             </div>
+        </div>
+             </div>
         </div>
     </div>
 @endsection
