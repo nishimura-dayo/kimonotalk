e5c4715b43f8a2be70bb32d4955c870c6a9a7d5a
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-10 09:46:51 +0000

    Topic list page change　design

--- resources/views/topics/form_topic.blade.php
+++ resources/views/topics/form_topic.blade.php
@@ -3,10 +3,12 @@
 
 {!! Form::open(['route' => 'topics.store','enctype'=>'multipart/form-data']) !!}
     <div class="form-graup">
-        {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '2']) !!}
-        <!-- アップロードフォームの作成 -->
-        {!! Form::file('image_path') !!}
-        @csrf
-        {!! Form::submit('トピックを投稿する', ['class' => 'btn btn-primary btn-block']) !!}
+        {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '3', 'placeholder' => '気になることを書いてみてね♪']) !!}
+        <div class="toolbar">
+            <!-- アップロードフォームの作成 -->
+            {!! Form::file('image_path', ['class' => 'btn btn-upload']) !!}
+            @csrf
+            {!! Form::submit('トピックを投稿する', ['class' => 'btn btn-submit-post']) !!}
+        </div>
     </div>
 {!! Form::close() !!}
