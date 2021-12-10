3c3df66248cc60a52c1adb1a3eb1939a043cd7b7
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-10 06:44:33 +0000

    Post details design changes

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -2,46 +2,56 @@
 
 @section('content')
 
-    <div class="users">
+    <div class="topic-single">
         <h1 class="headline">トピック詳細ページ</h1>
 
         <section class="container">
             <div class="row">
                 <div class="col-sm-8 offset-sm-2">
-                    <div class="row">
-                        <div class="col-3">
-                            {{-- トピック画像 --}}
-                            <img src=" {{ $topic->image_path }}" alt="">
-                        </div>
-                        <div class="col-9">
-                            <div class="row">
-                                <div class="col-2">
-                                    {{-- トピック所有者のメールアドレスをもとにGravatarを取得して表示 --}}
-                                    <div  class="user-thumbnail"><img src="{{ Gravatar::get($topic->user->email, ['size' => 120]) }}" alt=""></div>
-                                </div>
-                                <div class="col-10">
-                                    <div>
-                                        {{-- トピック所有者のユーザ詳細ページへのリンク --}}
-                                        {!! link_to_route('users.show', $topic->user->name, ['user' => $topic->user->id], ['class' => 'text_link']) !!}
-                                    </div>
-                                    {{-- トピック日時 --}}
-                                    <span class="text-muted">{{ $topic->created_at }}</span>
-                                </div>
-                            </div>
-                            <div class="topic_content">
-                                <div class="topic_text">
-                                    {{-- トピック詳細--}}
-                                    {{ $topic->content }}
-                                </div>
-                                <div class="text-right">
-                                    @if (Auth::id() == $topic->user_id)
-                                        {{-- トピック削除ボタンのフォーム --}}
-                                        {!! Form::open(['route' => ['topics.destroy', $topic->id], 'method' => 'delete']) !!}
-                                            {!! Form::submit('削除する', ['class' => 'btn btn-danger btn-sm']) !!}
-                                        {!! Form::close() !!}
+                    {{-- トピック --}}
+                    <div class="topic-detail">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="topic-image">
+                                    @if(isset( $topic->image_path ))
+                                        {{-- トピック画像がある場合 --}}
+                                        <img src=" {{ $topic->image_path }}" alt="">
+                                    @else
+                                        {{-- トピック画像がない場合 --}}
+                                        <img src="{{ asset('image/no-image.png') }}" alt="">
                                     @endif
                                 </div>
                             </div>
+                            <div class="col-9">
+                                <div class="row">
+                                    <div class="col-2">
+                                        {{-- トピック所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                                        <div  class="user-thumbnail"><img src="{{ Gravatar::get($topic->user->email, ['size' => 120]) }}" alt=""></div>
+                                    </div>
+                                    <div class="col-10">
+                                        <div>
+                                            {{-- トピック所有者のユーザ詳細ページへのリンク --}}
+                                            {!! link_to_route('users.show', $topic->user->name, ['user' => $topic->user->id], ['class' => 'topic-user-name']) !!}
+                                        </div>
+                                        {{-- トピック日時 --}}
+                                        <span class="text-muted">{{ $topic->created_at }}</span>
+                                    </div>
+                                </div>
+                                <div class="topic_content">
+                                    <div class="topic_text">
+                                        {{-- トピック詳細--}}
+                                        {{ $topic->content }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="toolbar">
+                            @if (Auth::id() == $topic->user_id)
+                                {{-- トピック削除ボタンのフォーム --}}
+                                {!! Form::open(['route' => ['topics.destroy', $topic->id], 'method' => 'delete']) !!}
+                                    {!! Form::submit('トピックを削除する', ['class' => 'btn btn-submit-post']) !!}
+                                {!! Form::close() !!}
+                            @endif
                         </div>
                     </div>
                     {{-- コメントフォーム --}}
