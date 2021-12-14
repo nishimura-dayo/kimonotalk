2a1600da76fed3a976afc30ee3406abd6ad6871d
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-14 13:03:00 +0000

    category model

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['content'];
+
+    /**
+     * このカテゴリが所有するトピック(Topicモデルとの関係を定義)
+     */
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+    
+    /**
+     * このカテゴリが所有するコメント(Commentモデルとの関係を定義)
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
