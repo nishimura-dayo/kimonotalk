578e3cc514c363f7e550b532aa00f877a7fd36f9
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2021-12-16 10:29:29 +0000

    topPage category→categories

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,35 @@
     @if(Auth::check())
         <div class="users container">
             <h1 class="headline">トップページ（ログイン中）</h1>
+            <section class="home-menu">
+            <h2 class="home-title">カテゴリから探す</h2>
+            <div class="home-menu-list">
+                <dl class="home-menu-list-item">
+                    <dt class="home-category-name"><a href="/categories/1" class="home-menu-btn1">着物ショップ</a></dt>
+                    <dd class="home-category-text">安心してお買い物しましょ</dd>
+                </dl>
+                <dl class="home-menu-list-item">
+                    <dt class="home-category-name"><a href="/categories/2" class="home-menu-btn2">着物コーデ</a></dt>
+                    <dd class="home-category-text">このコーディネートが好き</dd>
+                </dl>
+                <dl class="home-menu-list-item">
+                    <dt class="home-category-name"><a href="/categories/3" class="home-menu-btn3">着付けテクニック</a></dt>
+                    <dd class="home-category-text">簡単に着る方法ないですか？</dd>
+                </dl>
+                <dl class="home-menu-list-item">
+                    <dt class="home-category-name"><a href="/categories/4" class="home-menu-btn4">着物姿の芸能人</a></dt>
+                    <dd class="home-category-text">おしゃれな芸能人探し</dd>
+                </dl>
+                <dl class="home-menu-list-item">
+                    <dt class="home-category-name"><a href="/categories/5" class="home-menu-btn5">着物警察</a></dt>
+                    <dd class="home-category-text">ある日突然警察に襲われた</dd>
+                </dl>
+                <dl class="home-menu-list-item">
+                    <dt class="home-category-name"><a href="/categories/6" class="home-menu-btn6">お手入れ・収納</a></dt>
+                    <dd class="home-category-text">ドライクリーニングしか駄目？</dd>
+                </dl>
+            </div>
+        </section>
             <div class="row">
                 <div class="col-sm-8">
                     {{-- トピック一覧 --}}
@@ -23,27 +52,27 @@
             <h2 class="home-title">カテゴリから探す</h2>
             <div class="home-menu-list">
                 <dl class="home-menu-list-item">
-                    <dt class="home-category-name"><a href="/category/1" class="home-menu-btn1">着物ショップ</a></dt>
+                    <dt class="home-category-name"><a href="/categories/1" class="home-menu-btn1">着物ショップ</a></dt>
                     <dd class="home-category-text">安心してお買い物しましょ</dd>
                 </dl>
                 <dl class="home-menu-list-item">
-                    <dt class="home-category-name"><a href="/category/2" class="home-menu-btn2">着物コーデ</a></dt>
+                    <dt class="home-category-name"><a href="/categories/2" class="home-menu-btn2">着物コーデ</a></dt>
                     <dd class="home-category-text">このコーディネートが好き</dd>
                 </dl>
                 <dl class="home-menu-list-item">
-                    <dt class="home-category-name"><a href="/category/3" class="home-menu-btn3">着付けテクニック</a></dt>
+                    <dt class="home-category-name"><a href="/categories/3" class="home-menu-btn3">着付けテクニック</a></dt>
                     <dd class="home-category-text">簡単に着る方法ないですか？</dd>
                 </dl>
                 <dl class="home-menu-list-item">
-                    <dt class="home-category-name"><a href="/category/4" class="home-menu-btn4">着物姿の芸能人</a></dt>
+                    <dt class="home-category-name"><a href="/categories/4" class="home-menu-btn4">着物姿の芸能人</a></dt>
                     <dd class="home-category-text">おしゃれな芸能人探し</dd>
                 </dl>
                 <dl class="home-menu-list-item">
-                    <dt class="home-category-name"><a href="/category/5" class="home-menu-btn5">着物警察</a></dt>
+                    <dt class="home-category-name"><a href="/categories/5" class="home-menu-btn5">着物警察</a></dt>
                     <dd class="home-category-text">ある日突然警察に襲われた</dd>
                 </dl>
                 <dl class="home-menu-list-item">
-                    <dt class="home-category-name"><a href="/category/6" class="home-menu-btn6">お手入れ・収納</a></dt>
+                    <dt class="home-category-name"><a href="/categories/6" class="home-menu-btn6">お手入れ・収納</a></dt>
                     <dd class="home-category-text">ドライクリーニングしか駄目？</dd>
                 </dl>
             </div>
