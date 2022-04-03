cf2fff9da4efbb1f7297283021fdbc8b382ff9cd
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2022-04-03 02:48:47 +0000

    add Laravel Mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <script src="{{ asset('/js/jquery.js') }}"></script>
     </head>
 
     <body>
